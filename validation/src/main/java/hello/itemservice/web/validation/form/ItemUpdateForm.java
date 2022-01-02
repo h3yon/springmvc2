e0c2ec6ed76e99ac6a33cef21ca3aee219a14bc9
h3yon <khykhy1006@gmail.com>
2022-01-02 15:29:36 +0900

    4: Bean Validation 수정/등록 폼 분리

---
+++ validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,28 @@
+package hello.itemservice.web.validation.form;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import lombok.Data;
+
+@Data
+public class ItemUpdateForm {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    //수정에서는 수량 자유롭게
+    private Integer quantity;
+}
